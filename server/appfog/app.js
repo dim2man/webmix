992911777dea9cc491659e8c130c224fc3aa4dad
Dmitry Manzhasov <dim2man@gmail.com>
2013-07-10 19:22:02 +0400

    started adding WebSockets server for chat

--- server/appfog/app.js
+++ server/appfog/app.js
@@ -1,5 +1,34 @@
 var http = require('http');
-http.createServer(function (req, res) {
+var ws = require('websocket');
+
+var wsPort = 8080;
+
+var wsHttpServer = http.createServer(function(request, response) {
+  response.writeHead(404, {"X-WebSocket-Port": wsPort});
+  response.end();
+});
+wsHttpServer.listen(wsPort, null);
+
+var wsServer = new (ws.server)({
+    httpServer: wsHttpServer,
+    autoAcceptConnections: true
+});
+
+wsServer.on('connect', function(connection) {
+    var id = connection.socket.remoteAddress + ':' + connection.socket.remotePort;
+    wsServer.broadcastUTF(id + ' connected');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            wsServer.broadcastUTF(id + ': ' + message.utf8Data);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        wsServer.broadcastUTF(id + ' disconnected');
+    });
+});
+
+var httpServer = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello from <a href="http://appfog.com">AppFog.com</a>');
-}).listen(process.env.VMC_APP_PORT || 1337, null);
+  res.end('wsServer is '+wsServer);
+}).listen(8081, null);
+
